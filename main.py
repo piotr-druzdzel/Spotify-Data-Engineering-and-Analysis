2e42ceb5b22d86b41437555f2a32f01f4f9302fe
piotrdruzdzel <piotrdruzdzel@gmail.com>
2022-12-02 18:27:58 +0100

    token

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 # token
 # BQDwDCa79PuhtU2BVMl_dbYSm4-A_kk6-dzFoxIbqiR8KEsKlYWaEhiLnDcD4g69W3v9mjom44VFqZBsZYsJDvxSdRg8e9wJCAaNtcPhgSEElTUhmIglDquMFeMBXxl96fJbsmZtIghFbwIczVAWTbdWPa9ZtEWdVotH7h0XPwnzfikqnNj4ww
-
-#ok
