6ffc2b485fce796825c2f88aa2e04d2ba98b4a0e
piotrdruzdzel <piotrdruzdzel@gmail.com>
2022-12-02 20:13:26 +0100

    imports addes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Spotify token
 # BQDwDCa79PuhtU2BVMl_dbYSm4-A_kk6-dzFoxIbqiR8KEsKlYWaEhiLnDcD4g69W3v9mjom44VFqZBsZYsJDvxSdRg8e9wJCAaNtcPhgSEElTUhmIglDquMFeMBXxl96fJbsmZtIghFbwIczVAWTbdWPa9ZtEWdVotH7h0XPwnzfikqnNj4ww
+
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
+
