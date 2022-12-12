d353da6faac4ea2722b6ebdf6b50740358d7d1fc
piotrdruzdzel <piotrdruzdzel@gmail.com>
2022-12-12 23:53:57 +0100

    add default argumentgs to pass to airflow for the configuration

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -3,3 +3,14 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 12, 12),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
